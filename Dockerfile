fed859d7979dd4d0083e6bcdb27d003b20a91e7c
Jeff Newman <jeff@driftless.xyz>
2024-01-17 16:24:46 -0600

    test fix

---
+++ Dockerfile
@@ -0,0 +1,60 @@
+FROM mambaorg/micromamba:1.5.6 AS build-stage
+ARG MAMBA_DOCKERFILE_ACTIVATE=1
+
+# listing all the builder environment packages literally in this dockerfile
+# instead of using a seperate definitions file ensures that the image does
+# not need to be rebuilt unless the packages actually change (i.e. not if
+# the external file appears to have a recent file modification date but wasn't
+# actually edited).  This list also should only included the minimum set of
+# packages to actually build and test the core, not to run everything a developer
+# might touch (e.g. skip docs, jupyter, and add-on tools).
+RUN micromamba install -y -n base -c conda-forge \
+    "python=3.10" \
+    pip \
+    "numpy>=1.19" \
+    "pandas>=2.1" \
+    pyarrow \
+    xarray \
+    dask \
+    networkx \
+    "numba>=0.53" \
+    numexpr \
+    sparse \
+    filelock \
+    addicty \
+    xmle \
+    rich \
+    altair \
+    altair_saver \
+    pydot \
+    platformdirs \
+    tabulate \
+    pytest \
+    pytest-regressions \
+    pytest-xdist \
+    nbmake \
+    openmatrix \
+    zarr \
+    geopandas \
+    sharrow \
+    && \
+    micromamba clean --all --yes \
+    && \
+    python -m pip install jax jaxlib xlogit --no-cache-dir
+
+ENV PYTHONUNBUFFERED=1
+
+# Custom cache invalidation
+ARG CACHEBUST=1
+
+# copy the repo contents into the docker container
+COPY --chown=$MAMBA_USER:$MAMBA_USER . /tmp/larix/larch
+
+# Compile wheel
+RUN python -m pip wheel --wheel-dir=/tmp/wheelhouse --no-deps -v /tmp/larix/larch
+
+# Install from wheel
+RUN python -m pip install --no-index --find-links=/tmp/wheelhouse larch
+
+# Run tests
+RUN python -m pytest -v /tmp/larix/larch --ignore=/tmp/larix/larch/sandbox
---
+++ tests/test_swissmetro.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from pytest import approx
+
+import larch as lx
+from larch import P, X
+
+
+def test_swissmetro_101():
+    m = lx.Model()
+    m.title = "swissmetro example 01 (simple logit)"
+    m.availability_co_vars = {
+        1: "TRAIN_AV * (SP!=0)",
+        2: "SM_AV",
+        3: "CAR_AV * (SP!=0)",
+    }
+    m.choice_co_code = "CHOICE"
+    m.utility_co[1] = P("ASC_TRAIN")
+    m.utility_co[2] = 0
+    m.utility_co[3] = P("ASC_CAR")
+    m.utility_co[1] += X("TRAIN_TT") * P("B_TIME")
+    m.utility_co[2] += X("SM_TT") * P("B_TIME")
+    m.utility_co[3] += X("CAR_TT") * P("B_TIME")
+    m.utility_co[1] += X("TRAIN_CO*(GA==0)") * P("B_COST")
+    m.utility_co[2] += X("SM_CO*(GA==0)") * P("B_COST")
+    m.utility_co[3] += X("CAR_CO") * P("B_COST")
+    m.ordering = [("ASCs", "ASC.*"), ("LOS", "B_.*")]
+    raw_data = pd.read_csv(lx.example_file("swissmetro.csv.gz")).rename_axis(
+        index="CASEID"
+    )
+    raw_data.head()
+    keep = raw_data.eval("PURPOSE in (1,3) and CHOICE != 0")
+    selected_data = raw_data[keep]
+    ds = lx.Dataset.construct.from_idco(
+        selected_data, alts={1: "Train", 2: "SM", 3: "Car"}
+    )
+    m.datatree = ds
+    m.set_cap(15)
+    result = m.maximize_loglike(method="SLSQP")
+    assert result.loglike == approx(-5331.252006971916)
+    m.calculate_parameter_covariance()
+    m.parameter_summary()
+    assert m.pvals == approx([-0.15475, -0.70178, -0.010842, -0.012776], rel=1e-3)
+    assert m.pstderr == approx(
+        [0.0432357, 0.0548783, 0.000518335, 0.000568829], rel=1e-3
+    )
+    assert m.parameter_summary().data["t Stat"].values.astype(float) == approx(
+        [-3.58, -12.79, -20.92, -22.46], rel=1e-2
+    )
