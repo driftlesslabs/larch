d94e3a64aeb4d23c27ab83e3defebc5aba0549d9
Jeff Newman <jeff@driftless.xyz>
2024-11-02 14:23:24 -0500

    install development dependencies

--- envs/uv-activitysim.sh
+++ envs/uv-activitysim.sh
@@ -107,6 +107,13 @@ uv run ./larch/tools/rip_examples.py
 uv pip install -e ./larch
 uv pip install -e ./activitysim
 
+# install development dependencies
+uv pip install altair asv "black<23" cytoolz dask filelock geopandas isort \
+  matplotlib myst-parser nbconvert nbformat nbmake numpydoc psutil pyarrow \
+  pycodestyle pydata-sphinx-theme pyinstrument pypyr pytest pytest-regressions \
+  rich ruby setuptools_scm scikit-learn simwrapper sparse sphinx sphinx_rtd_theme \
+  sphinx-argparse zarr zstandard
+
 cd larch
 uvx pre-commit install
 cd ..
